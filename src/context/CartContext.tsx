680cda712801dbfd2ddb981a978e83be2070ea38
fernandoprestes <fernandoprestesc@gmail.com>
2023-03-26 10:45:59 -0300

    add useMemo

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode, useState } from 'react';
+import { createContext, ReactNode, useCallback, useMemo, useState } from 'react';
 import { CartItem } from '~/@types/CartItem';
 import { produce } from 'immer';
 
@@ -25,69 +25,76 @@ export function CartContextProvider({ children }: CartContextProviderProps) {
     return total + cartItem.price * cartItem.quantity;
   }, 0);
 
-  function addCoffeeToCart(coffee: CartItem) {
-    const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === coffee.id);
+  const addCoffeeToCart = useCallback(
+    (coffee: CartItem) => {
+      const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === coffee.id);
 
-    const newCart = produce(cartItems, draft => {
-      if (hasCoffeeInCart < 0) {
-        draft.push(coffee);
-        return;
-      }
-
-      draft[hasCoffeeInCart].quantity += coffee.quantity;
-    });
-
-    setCartItems(newCart);
-  }
-
-  function changeCartItemQuantity(cartItemId: number, type: 'increase' | 'decrease') {
-    const newCart = produce(cartItems, draft => {
-      const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId);
-
-      if (hasCoffeeInCart >= 0) {
-        const item = draft[hasCoffeeInCart];
-
-        if (type === 'increase' && item.quantity > 0) {
-          draft[hasCoffeeInCart].quantity += 1;
+      const newCart = produce(cartItems, draft => {
+        if (hasCoffeeInCart < 0) {
+          draft.push(coffee);
           return;
         }
-        if (type === 'decrease' && item.quantity > 1) {
-          draft[hasCoffeeInCart].quantity -= 1;
-          return;
+
+        draft[hasCoffeeInCart].quantity += coffee.quantity;
+      });
+
+      setCartItems(newCart);
+    },
+    [cartItems],
+  );
+
+  const changeCartItemQuantity = useCallback(
+    (cartItemId: number, type: 'increase' | 'decrease') => {
+      const newCart = produce(cartItems, draft => {
+        const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId);
+
+        if (hasCoffeeInCart >= 0) {
+          const item = draft[hasCoffeeInCart];
+
+          if (type === 'increase' && item.quantity > 0) {
+            draft[hasCoffeeInCart].quantity += 1;
+            return;
+          }
+          if (type === 'decrease' && item.quantity > 1) {
+            draft[hasCoffeeInCart].quantity -= 1;
+            return;
+          }
         }
-      }
-    });
-    setCartItems(newCart);
-  }
+      });
+      setCartItems(newCart);
+    },
+    [cartItems],
+  );
 
-  function removeCartItem(cartItemId: number) {
-    const newCart = produce(cartItems, draft => {
-      const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId);
+  const removeCartItem = useCallback(
+    (cartItemId: number) => {
+      const newCart = produce(cartItems, draft => {
+        const hasCoffeeInCart = cartItems.findIndex(cartItem => cartItem.id === cartItemId);
 
-      if (hasCoffeeInCart >= 0) {
-        draft.splice(hasCoffeeInCart, 1);
-      }
-    });
-    setCartItems(newCart);
-  }
+        if (hasCoffeeInCart >= 0) {
+          draft.splice(hasCoffeeInCart, 1);
+        }
+      });
+      setCartItems(newCart);
+    },
+    [cartItems],
+  );
 
   function cleanCart() {
     setCartItems([]);
   }
-
-  return (
-    <CartContext.Provider
-      value={{
-        cartItems,
-        cartQuantity,
-        cartItemsTotal,
-        addCoffeeToCart,
-        changeCartItemQuantity,
-        removeCartItem,
-        cleanCart,
-      }}
-    >
-      {children}
-    </CartContext.Provider>
+  const cartContextMemo = useMemo(
+    () => ({
+      cartItems,
+      cartQuantity,
+      cartItemsTotal,
+      addCoffeeToCart,
+      changeCartItemQuantity,
+      removeCartItem,
+      cleanCart,
+    }),
+    [addCoffeeToCart, cartItems, cartItemsTotal, cartQuantity, changeCartItemQuantity, removeCartItem],
   );
+
+  return <CartContext.Provider value={cartContextMemo}>{children}</CartContext.Provider>;
 }
